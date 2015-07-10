a36c41afd489848abeeabae1977bcdb8faa5cd7d
Jackie Turner <turnerjackie425@gmail.com>
2015-07-10 14:19:07 -0600

    CP 37 complete -sendgrid issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704175639) do
+ActiveRecord::Schema.define(version: 20150710200024) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150704175639) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
