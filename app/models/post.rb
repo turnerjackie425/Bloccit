834076f90d1d1bcd4130ccb24c2bdf96e776e038
Jackie Turner <turnerjackie425@gmail.com>
2015-07-16 08:54:38 -0600

    CP 38 assignment progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC')}
+  # default_scope { order('created_at DESC')}
+  scope :ordered_by_title
+  scope :ordered_by_reverse_created_at
 end
+
+Post.ordered_by_title
+Post.ordered_by_reverse_created_at
