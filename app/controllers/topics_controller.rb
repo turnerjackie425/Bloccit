6ecde7b35f7b4e6f76b82525cff9d8c069941af2
Jackie Turner <turnerjackie425@gmail.com>
2015-07-21 13:58:47 -0600

    Complete interlude assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
   end
  
    def create
-     @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+     @topic = Topic.new(topics_params)
      authorize @topic
      if @topic.save
        redirect_to @topic, notice: "Topic was saved successfully."
@@ -34,11 +34,15 @@ class TopicsController < ApplicationController
    def update
      @topic = Topic.find(params[:id])
      authorize @topic
-     if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+     if @topic.update_attributes(topics_params)
        redirect_to @topic
      else
        flash[:error] = "Error saving topic. Please try again."
        render :edit
      end
    end
+
+    def topics_params
+    params.require(:topics).permit(:title, body)
+  end 
 end
