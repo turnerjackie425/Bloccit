8e43f0aaaacbe1bff81ad706610ca1737e119742
Jackie Turner <turnerjackie425@gmail.com>
2015-07-22 10:20:39 -0600

    CP 43 assignment complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,4 +62,21 @@ skip_before_action :flash_attack, only: [:index, :new]
     params.require(:post).permit(:title, body)
   end 
 
+  def markdown_title
+    render_as_markdown
+  end
+
+  def markdown_body
+    render_as_markdown
+  end 
+
+  
+ private
+
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
