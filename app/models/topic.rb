ffa84500f4da45795bba6ab4f191182b86c49d25
Jackie Turner <turnerjackie425@gmail.com>
2015-07-27 10:37:03 -0600

    progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,5 @@ class Topic < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
 
-  will_paginate(page: params[:page], per_page: 50)
+  WillPaginate.per_page = 50
 end
