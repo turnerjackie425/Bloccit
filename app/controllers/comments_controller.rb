269087d1bfd981ca05a045b3e3befd179e9ef2d0
Jackie Turner <turnerjackie425@gmail.com>
2015-07-28 09:20:07 -0600

    CP 46 progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,18 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new if current_user?
-    @post = Post.find(params[:id])
+    @comment = Comment.new(comment_params)
+    @post= Post.find(params[:post_id])
+    @comment = Comment.new(params.require(:post))
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Comment was saved."
+      redirect_to [@comment, @post]
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
   end
+
+  def comment_params
+    params.require(:comment).permit(body: string)
+  end 
+  end 
 end
