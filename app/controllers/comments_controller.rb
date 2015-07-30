052d5db483a54f07ac6a216dabc5074091bb10a5
Jackie Turner <turnerjackie425@gmail.com>
2015-07-30 10:23:22 -0600

    Destroy assignment complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,20 +15,19 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post]
     end
   end
-  
+
 
   def comment_params
     params.require(:comment).permit(body: string)
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:update]
-  resources :advertisements
-  resources :posts, only: [:index]
-
-  resources :summaries
-
-  resources :topics
-
+  
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only [:create, :destroy]
-    end
-  end
-
+     resources :posts, except: [:index]
+   end
+ 
+   resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
   get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
