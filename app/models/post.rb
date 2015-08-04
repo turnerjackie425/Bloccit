bfa46f7cd82e76e5033a4fde664db8d1bdc4a628
Jackie Turner <turnerjackie425@gmail.com>
2015-08-04 15:21:37 -0600

    progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,14 +32,12 @@ class Post < ActiveRecord::Base
     sum = up_votes + down_votes
   end
 
-
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
     new_rank = points + age_in_days
  
     update_attribute(:rank, new_rank)
   end
-
 
   def create_vote
     user.votes.create(value: 1, post: self)
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,13 @@ end
 users = User.all
 
 # Create Topics
-15.times do(
-    name:   Faker::Lorem.sentence,
-    description: Faker::Lorem.paragraph
-    )
-end
-topics = Topic.all
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  
  # Create Posts
 50.times do
